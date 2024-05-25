5cc0b3559f779dc814bf5d70fb9ad111553f3306
ErmiyasTesfaye <ermiyastesfaye16@gmail.com>
2024-05-25 10:19:43 +0300

    backend with rolebased authorization

--- src/crop/crop.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CropController } from './crop.controller';
-import { CropService } from './crop.service';
-
-@Module({
-  controllers: [CropController],
-  providers: [CropService]
-})
-export class CropModule {}
